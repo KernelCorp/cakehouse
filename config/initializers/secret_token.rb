ad97940971b45630ed07214c68b2b5e26f36dfe4
max-konin <mk@kernel-corp.com>
2014-01-29 16:59:18 +0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cakehouse::Application.config.secret_token = '6c38cb792c0a79e69ba8b83def081755bb079617c845f3225499fdec677a1606d865bf13ae8e61b0de524af1bfbf7046725f009449b90a5bc099d6a6fd16510e'
