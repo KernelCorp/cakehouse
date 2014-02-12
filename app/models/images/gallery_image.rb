68397a272e7c3e8acef3fdc06cf7d2515eb26fd6
mitchenkod <mitchenko1897@mail.ru>
2014-02-12 13:13:29 +0700

    random names for gallery images

--- app/models/images/gallery_image.rb
+++ app/models/images/gallery_image.rb
@@ -1,3 +1,15 @@
 class GalleryImage < Image
 
+  before_create :set_file_name
+
+  protected
+
+  def set_file_name
+    begin
+      name = SecureRandom.uuid
+    end while not Image.where(:attachment_file_name => name).empty?
+    extension = self.attachment_content_type.gsub('image/', '.')
+    self.attachment.instance_write(:file_name, name+extension)
+  end
+
 end
