de55e0de2409b387b7fb4ea4baa7cace43950382
max <mk@kernel-corp.com>
2014-05-26 16:31:21 +0700

    revers feedback and images

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,10 +1,10 @@
 class HomepageController < ApplicationController
 
   def index
-    @menu = Page.all.map {|page| OpenStruct.new href: page_path(page), title: page.title}
-    @feedbacks = Feedback.all
-    @cake_images = GalleryImage.all
-    @sale = Sale.last
+    @menu        = Page.all.map {|page| OpenStruct.new href: page_path(page), title: page.title}
+    @feedbacks   = Feedback.all.reverse
+    @cake_images = GalleryImage.all.reverse
+    @sale        = Sale.last
   end
 
 end
